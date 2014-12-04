c71506b35b2751810c9ca2e05efcd8715478ab70
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2014-12-04 09:27:45 +0900

    go fmt

--- handler.go
+++ handler.go
@@ -9,7 +9,7 @@ import (
 	"sync"
 	"time"
 )
- 
+
 type Stats struct {
 	Time int64 `json:"time"`
 	// runtime
@@ -44,61 +44,61 @@ type Stats struct {
 	GcPausePerSecond float64   `json:"gc_pause_per_second"`
 	GcPause          []float64 `json:"gc_pause"`
 }
- 
+
 var lastSampleTime time.Time
 var lastPauseNs uint64 = 0
 var lastNumGc uint32 = 0
- 
+
 var nsInMs float64 = float64(time.Millisecond)
- 
+
 var statsMux sync.Mutex
- 
+
 func GetStats() *Stats {
 	statsMux.Lock()
 	defer statsMux.Unlock()
- 
+
 	var mem runtime.MemStats
 	runtime.ReadMemStats(&mem)
- 
+
 	now := time.Now()
- 
+
 	var gcPausePerSecond float64
- 
+
 	if lastPauseNs > 0 {
 		pauseSinceLastSample := mem.PauseTotalNs - lastPauseNs
 		gcPausePerSecond = float64(pauseSinceLastSample) / nsInMs
 	}
- 
+
 	lastPauseNs = mem.PauseTotalNs
- 
+
 	countGc := int(mem.NumGC - lastNumGc)
- 
+
 	var gcPerSecond float64
- 
+
 	if lastNumGc > 0 {
 		diff := float64(countGc)
 		diffTime := now.Sub(lastSampleTime).Seconds()
 		gcPerSecond = diff / diffTime
 	}
- 
+
 	gcPause := make([]float64, countGc)
- 
+
 	if countGc > 0 {
 		if countGc > 256 {
 			// lagging GC pause times
 			countGc = 256
 		}
- 
+
 		for i := 0; i < countGc; i++ {
 			idx := int((mem.NumGC-uint32(i))+255) % 256
 			pause := float64(mem.PauseNs[idx])
 			gcPause[i] = pause / nsInMs
 		}
 	}
- 
+
 	lastNumGc = mem.NumGC
 	lastSampleTime = time.Now()
- 
+
 	return &Stats{
 		Time:         now.UnixNano(),
 		GoVersion:    runtime.Version(),
